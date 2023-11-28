7cd1b5b67338f357512018f7dc639736bc81260b
Đỗ Ngọc Huy <99071294+huytruetheblue@users.noreply.github.com>
2023-11-28 22:03:57 +0700

    Update news url and add data parsing logic (generated by blackbox.ai )

--- news_crawler/spiders/news_spider.py
+++ news_crawler/spiders/news_spider.py
@@ -1,21 +1,34 @@
 import scrapy
-
+import re
+from scrapy.spiders import  Rule
+from scrapy.linkextractors import LinkExtractor
 class NewsSpider(scrapy.Spider):
     name = "news"
     
+    allowed_domains = ['www.bbc.com']
     start_urls = [
-        "https://www.bbc.com/sport/football/67500698",
+        "https://www.bbc.com/sport/football/67470815",
     ]
+    
+    rules = (
+        Rule(
+            LinkExtractor(allow=r'/sport/football/\d+$', deny=r'/sport/football/$'),
+            callback="parse_item",
+            follow=True
+        ),
+    )
+
 
     def parse(self, response):
-            # yield {
-            #     "title": quote.xpath("//h1/text()").get(),
-            #     "time": quote.xpath("//time//span/text()").get() 
-            # }\
+        football_links = response.css('a.gs-c-promo-heading').getall()
+        yield from response.follow_all(football_links, self.parse_author)
+    
+    def parse_football(sefl, response):
         content = response.xpath("//p//span/text()").getall()
         content = " ".join(content)
         yield {
             "title": response.xpath("//h1/text()").get(),
             "time": response.xpath("//time//span/text()").get(),
-            "content": content
+            "content": content,
+            "url": response.url,
         }
