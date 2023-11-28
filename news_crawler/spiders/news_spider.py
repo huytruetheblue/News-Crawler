790e380cc8cab3ceee11806dceff17d91c2fd609
Đỗ Ngọc Huy <99071294+huytruetheblue@users.noreply.github.com>
2023-11-28 23:53:40 +0700

    commit

--- news_crawler/spiders/news_spider.py
+++ news_crawler/spiders/news_spider.py
@@ -1,34 +1,42 @@
 import scrapy
 import re
-from scrapy.spiders import  Rule
-from scrapy.linkextractors import LinkExtractor
+from twisted.internet.error import ConnectionLost
+from scrapy.spidermiddlewares.httperror import HttpError
+from ..items import NewsCrawlerItem
 class NewsSpider(scrapy.Spider):
     name = "news"
+    
+    custom_settings = {
+        'RETRY_TIMES': 3,
+        'RETRY_DELAY': 5,
+        'DOWNLOAD_TIMEOUT': 30
+    }
     
     allowed_domains = ['www.bbc.com']
     start_urls = [
         "https://www.bbc.com/sport/football/67470815",
     ]
-    
-    rules = (
-        Rule(
-            LinkExtractor(allow=r'/sport/football/\d+$', deny=r'/sport/football/$'),
-            callback="parse_item",
-            follow=True
-        ),
-    )
 
 
     def parse(self, response):
-        football_links = response.css('a.gs-c-promo-heading').getall()
-        yield from response.follow_all(football_links, self.parse_author)
+        if isinstance(response.error, ConnectionLost):
+            # Thử lại kết nối
+            yield response.request.replace(dont_filter=True)
+        
+        news_links = response.css('a[href*="/sport/football/"]::attr(href)').getall()
+    
+        # Theo dõi các đường dẫn tin tức bóng đá
+        for link in news_links:
+            match = re.search(r'/(\d+)$', link)
+            if match:
+                yield response.follow(link, callback=self.parse_football)
     
     def parse_football(sefl, response):
         content = response.xpath("//p//span/text()").getall()
         content = " ".join(content)
-        yield {
-            "title": response.xpath("//h1/text()").get(),
-            "time": response.xpath("//time//span/text()").get(),
-            "content": content,
-            "url": response.url,
-        }
+        item = NewsCrawlerItem()
+        item["title"] = response.xpath("//h1/text()").get()
+        item["time"] = response.xpath("//time//span/text()").get()
+        item["content"] = content
+        item["url"] = response.url
+        yield item
